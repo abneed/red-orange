4b409dfbf15ec612190b984e449357bb400edc6c
Abneed <memoboy262@gmail.com>
2019-05-02 03:27:33 -0500

    Finish implement Skill model

---
+++ app/views/skills/_skill.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! skill, :id, :name, :description, :degree_of_difficulty, :created_at, :updated_at
+json.url skill_url(skill, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_233422) do
+ActiveRecord::Schema.define(version: 2019_05_02_064455) do
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "degree_of_difficulty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
